1ae785351d5a8dd2f380021d1b8071dcf595cadb
samschool789@gmail.com <samschool789@gmail.com>
2022-05-25 21:01:12 -0500

    variables for generator

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+const Manager = require("./lib/manager")
+const Engineer = require("./lib/Engineer")
+const Intern = require("./lib/intern")
 const inquirer = require('inquirer');
+const path = require("path")
 const fs = require('fs')
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const render = require("./lib/htmlRenderer");
+
+
+
+
+
+
+
 
 
 inquirer
