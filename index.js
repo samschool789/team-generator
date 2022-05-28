1acf8cb0fa3bd801d78f60f4749f0426a505ecad
samschool789@gmail.com <samschool789@gmail.com>
2022-05-27 20:11:37 -0500

    final code

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const render = require("./lib/htmlRenderer");
 
 
-const students = [];
+const teamMembers = [];
 
 const firstQuestion = {
     type: "list",
@@ -30,5 +30,153 @@ const firstQuestion = {
     name: "fileName",
   };
 
-
+  const Questions = {
+    Manager: [
+      {
+        type: "input",
+        message: "Please enter your name",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is your id Number?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is your email?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What is your office number?",
+        name: "officeNumber",
+      },
+    ],
+    Engineer: [
+      {
+        type: "input",
+        message: "Your name",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is your id Number?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is your email?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What is your Github userName?",
+        name: "githubUserName",
+      },
+    ],
+    Intern: [
+      {
+        type: "input",
+        message: "Please enter your name",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is your id Number?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is your email?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What is your school?",
+        name: "school",
+      },
+    ],
+  };
+  
+  
+  
+  const startApp = () => {
+    selectRole();
+  };
+  
+  const addOrFinish = () => {
+    inquirer.prompt(firstQuestion).then((answer) => {
+      if (answer.Add === "Add Member") {
+        selectRole();
+      } else {
+        //generateTeam();
+        getFileName();
+      }
+    });
+  };
+  
+  const selectRole = () => {
+    inquirer.prompt(questionsYourRole).then((answer) => {
+      console.log(answer);
+      roleQuestions(Questions[answer.role], answer.role);
+    });
+  };
+  const roleQuestions = (questions, role) => {
+    inquirer.prompt(questions).then((answer) => {
+      //console.log(answer);
+      let member = {};
+      if (role === "Manager") {
+        member = new Manager(
+          answer.name,
+          answer.id,
+          answer.email,
+          answer.officeNumber
+        );
+      } else if (role === "Engineer") {
+        member = new Engineer(
+          answer.name,
+          answer.id,
+          answer.email,
+          answer.gitHubUserName
+        );
+      } else if (role === "Intern") {
+        member = new Intern(answer.name, answer.id, answer.email, answer.school);
+      }
+      teamMembers.push(member);
+      addOrFinish();
+    });
+  };
+  console.log(teamMembers);
+  
+  const getFileName = () => {
+    inquirer.prompt(fileNameQuestion).then((answer) => {
+      if (answer.fileName) {
+        generateTeam(answer.fileName);
+      } else {
+        getFileName();
+      }
+    });
+  };
+  
+  const generateTeam = (fileName) => {
+    const outputPath = path.join(OUTPUT_DIR, fileName + ".html");
+  
+    if (!fs.existsSync(OUTPUT_DIR)) {
+      fs.mkdirSync(OUTPUT_DIR);
+    }
+  
+    fs.writeFileSync(outputPath, render(teamMembers), (err) => {
+      if (err) {
+        console.log(err);
+        getFileName();
+      }
+    });
+  };
+  
+  // const isFileNameValid = (fileName) => {
+  //   if(fileName === )
+  // }
+  
+  startApp();
 
